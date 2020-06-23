26b2891b058f700634ea67096b8605e417a47f19
botelja <ivo.surjak@gmail.com>
2020-06-23 11:40:57 +0200

    Create clinet side react app and made Navbar

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppNavbar from './components/layout/AppNavbar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/layout/AppNavbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+const AppNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <div>
+      <Navbar color="dark" dark>
+        <NavbarBrand href="/" className="mr-auto">
+          ShoppingList
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} className="mr-2" />
+        <Collapse isOpen={!isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="https://github.com/botelja/shopping-list">
+                Github
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default AppNavbar;
